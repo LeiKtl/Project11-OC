ae2f5be040f2a12de109bdec3d3a6b360d3074bc
LeiKtl <leilaktl.pro@gmail.com>
2024-02-20 15:45:15 +0100

    🐛 fix for by htmlFor

--- front/src/components/form/index.jsx
+++ front/src/components/form/index.jsx
@@ -1,22 +1,23 @@
 import './form.css';
+import { Link } from 'react-router-dom';
 
 function Form () {
     return (
         <form>
           <div className="input-wrapper">
-            <label for="username">Username</label>
+            <label htmlFor="username">Username</label>
             <input type="text" id="username" />
           </div>
           <div className="input-wrapper">
-            <label for="password">Password</label>
+            <label htmlFor="password">Password</label>
             <input type="password" id="password" />
           </div>
           <div className="input-remember">
             <input type="checkbox" id="remember-me" />
-            <label for="remember-me">Remember me</label>
+            <label htmlFor="remember-me">Remember me</label>
           </div>
           {/* <a href="./user.html" class="sign-in-button">Sign In</a> */}
-          <button className="sign-in-button">Sign In</button>
+          <Link to="/profile"><button className="sign-in-button">Sign In</button></Link>
         </form>
     )
 }
