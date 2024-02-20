270027656e15eb44e5fafd9ac7d6e9d2fbb76ce6
LeiKtl <leilaktl.pro@gmail.com>
2024-02-20 14:23:38 +0100

    🎨 Modify structure of the code to be able to show features's pictures

--- front/src/components/features/index.jsx
+++ front/src/components/features/index.jsx
@@ -1,11 +1,14 @@
 import './features.css';
+import useImage from '../../hooks/useImage';
 
 function Features (props) {
+  const image = useImage(props.src);
+
     return (
         <div className="feature-item">
-          <img src={props.src} alt={props.alt} className="feature-icon" />
-          <h3 className="feature-item-title">{props.title}</h3>
-          <p>{props.content}</p>
+          <img src={ image } alt={ props.alt } className="feature-icon" />
+          <h3 className="feature-item-title">{ props.title }</h3>
+          <p>{ props.content }</p>
         </div>
     )
 }
--- front/src/datas/featuresList.js
+++ front/src/datas/featuresList.js
@@ -3,19 +3,19 @@ export const featuresList =
     {
         "title" : "You are our #1 priority",
         "content" : "Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes.",
-        "src" : "../../assets/img/icon-chat.png",
+        "src" : "icon-chat.png",
         "alt" : "Chat Icon",
     },
     {
         "title" : "More savings means higher rates",
         "content" : "The more you save with us, the higher your interest rate will be!",
-        "src" : "../../assets/img/icon-money.png",
+        "src" : "icon-money.png",
         "alt" : "Chat Icon",
     },
     {
         "title" : "Security you can trust",
         "content" : "We use top of the line encryption to make sure your data and money is always safe.",
-        "src" : "../../assets/img/icon-security.png",
+        "src" : "icon-security.png",
         "alt" : "Chat Icon",
     }
 ]
