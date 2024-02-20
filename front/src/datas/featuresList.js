848c0ffd30d8a48a9d7420f2edcbbc8780e8387f
LeiKtl <leilaktl.pro@gmail.com>
2024-02-20 14:58:33 +0100

    🎨 Add key

--- front/src/datas/featuresList.js
+++ front/src/datas/featuresList.js
@@ -1,18 +1,21 @@
 export const featuresList = 
 [
     {
+        "id" : 1,
         "title" : "You are our #1 priority",
         "content" : "Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes.",
         "src" : "icon-chat.png",
         "alt" : "Chat Icon",
     },
     {
+        "id" : 2,
         "title" : "More savings means higher rates",
         "content" : "The more you save with us, the higher your interest rate will be!",
         "src" : "icon-money.png",
         "alt" : "Chat Icon",
     },
     {
+        "id" : 3,
         "title" : "Security you can trust",
         "content" : "We use top of the line encryption to make sure your data and money is always safe.",
         "src" : "icon-security.png",
