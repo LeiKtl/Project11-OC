02506bd2a946e43218ef642f5c46399672640085
LeiKtl <leilaktl.pro@gmail.com>
2024-02-20 14:29:05 +0100

    🎨 Add login integration and rename file

--- front/src/App.js
+++ front/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/header';
 import Home from './pages/home';
-import SignIn from './pages/signIn';
+import Login from './pages/login';
 import Footer from './components/footer';
 
 function App() {
@@ -11,7 +11,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} ></Route>
-        <Route path="/signIn" element={<SignIn />} ></Route>
+        <Route path="/signIn" element={<Login />} ></Route>
       </Routes>
       <Footer />
     </Router>
