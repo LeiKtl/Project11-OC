460b0cc8ca098fe0e7263e0ddaaf09f563912860
LeiKtl <leilaktl.pro@gmail.com>
2024-02-23 22:47:22 +0100

    🎨 Add condition to show logout on the profile when user is login

--- front/src/components/header/index.jsx
+++ front/src/components/header/index.jsx
@@ -3,6 +3,12 @@ import { Link } from 'react-router-dom';
 import logo from '../../assets/img/argentBankLogo.png'
 
 function Header () {
+    const token = localStorage.getItem("token")
+    
+    function handleLogout () {
+        localStorage.removeItem("token");
+    }
+
     return (
         <nav className="main-nav">
             <Link to='/' className="main-nav-logo">
@@ -13,11 +19,14 @@ function Header () {
                 />
                 <h1 className="sr-only">Argent Bank</h1>
             </ Link>
+            {token ? 
             <div>
-                <Link to='/signIn' className="main-nav-item">
-                <i className="fa fa-user-circle"></i> Sign In
-                </ Link>
+                <Link to='/signIn' className="main-nav-item" onClick={handleLogout}><i className="fa-solid fa-right-from-bracket"></i> Logout</ Link>
             </div>
+            : 
+            <div>
+                <Link to='/signIn' className="main-nav-item"><i className="fa fa-user-circle"></i> Sign In</ Link>
+            </div>}
         </nav>
     )
 }
