fb367f1c3174d30716599cb2c708f071579913c4
LeiKtl <leilaktl.pro@gmail.com>
2024-02-23 22:46:16 +0100

    🎨 Improve code by adding fetch to be able to login and logout

--- front/src/components/form/index.jsx
+++ front/src/components/form/index.jsx
@@ -1,23 +1,58 @@
+import { useNavigate } from 'react-router-dom';
 import './form.css';
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 function Form () {
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const navigate = useNavigate()
+
+  console.log(password)
+  console.log(email)
+
+  const user = {
+    email : email,
+    password : password
+  }
+
+  function handleSubmit (e) {
+    e.preventDefault();
+
+    fetch("http://localhost:3001/api/v1/user/login", {
+            method: "POST",
+            headers: {
+                "Accept" : "application/json",
+                "Content-Type" : "application/json",
+            },
+            body: JSON.stringify(user)
+        })
+    .then((response) => response.json())
+    .then((data) => {
+      const userInfo = data.body
+      if (userInfo.token) {
+        localStorage.setItem("token", userInfo.token);
+        navigate("/profile");
+      } else {
+        console.log("erreur")
+      }
+    })
+  }
+
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="input-wrapper">
-            <label htmlFor="username">Username</label>
-            <input type="text" id="username" />
+            <label htmlFor="username">Username/Email</label>
+            <input type="text" id="username" value={email} onChange={event => setEmail(event.target.value)}/>
           </div>
           <div className="input-wrapper">
             <label htmlFor="password">Password</label>
-            <input type="password" id="password" />
+            <input type="password" id="password" value={password} onChange={event => setPassword(event.target.value)}/>
           </div>
           <div className="input-remember">
             <input type="checkbox" id="remember-me" />
             <label htmlFor="remember-me">Remember me</label>
           </div>
-          {/* <a href="./user.html" class="sign-in-button">Sign In</a> */}
-          <Link to="/profile"><button className="sign-in-button">Sign In</button></Link>
+          <button type="submit" className="sign-in-button">Sign In</button>
         </form>
     )
 }
