243079a6df599a8d656d90a5780b1633273a49c4
LeiKtl <leilaktl.pro@gmail.com>
2024-02-27 15:23:27 +0100

    🎨 Adding user info with API data

--- front/src/pages/profile/index.jsx
+++ front/src/pages/profile/index.jsx
@@ -5,31 +5,61 @@ import FormUserEdit from '../../components/formUserEdit';
 
 function Profile () {
     const [isClosed, setIsClosed] = useState(true);
-    const [accounts, setAccounts] = useState([])
+    const [accounts, setAccounts] = useState([]);
+    const [data, setData] = useState([]);
+    const [username, setUsername] = useState('');
+
+    function fetchUserProfile () {
+      const token = localStorage.getItem("token");
+
+      fetch("http://localhost:3001/api/v1/user/profile", {
+        method: "POST",
+        headers: {
+            "Accept" : "application/json",
+            "Content-Type" : "application/json",
+            "Authorization" : `Bearer${token}`
+        }
+      })
+      .then((response) => response.json())
+      .then((data) => {
+          const user = data.body
+          setUsername(user.userName)
+          setData(user)
+      })
+    };
 
     function toggle () {
-        setIsClosed(!isClosed)
+        setIsClosed(!isClosed);
+    };
+
+    function handleSubmit () {
+      toggle();
+      fetchUserProfile ();
+    };
+
+    function fetchUserAccounts () {
+      fetch("/accountData/accountsInfo.json")
+        .then((response) => response.json())
+        .then((dataAccounts) => {
+            setAccounts(dataAccounts)
+        })
     };
 
     useEffect(() => {
-      fetch("/accountData/accountsInfo.json")
-      .then((response) => response.json())
-      .then((dataAccounts) => {
-          setAccounts(dataAccounts)
-      })
-  }, []);
+      fetchUserAccounts();
+      fetchUserProfile();
+    }, []);
 
     return (
     <main className="main bg-dark">
       <div className="header">
-        {/* changer nom selon user avec API */}
         {isClosed ? 
         <>
         <h1 className="profile-welcome-text">Welcome back</h1>
-        <h2 className="profile-name">Tony Jarvis!</h2>
-        {/* changer nom avec Username par API */}
-        <button className="edit-button" onClick={toggle}>Edit Name</button></> 
-        : <FormUserEdit />}
+        <h2 className="profile-name">{username}!</h2>
+        <button className="edit-button" onClick={toggle}>Edit Name</button>
+        </> 
+        : <FormUserEdit data={data} onSuccess={handleSubmit} close={toggle}/>}
       </div>
       <h2 className="sr-only">Accounts</h2>
       {accounts.map(({id, title, amount, description}) => (
